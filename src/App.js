f4a938ce8e18c8cf7242bbcfee47e315f61337f0
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-17 15:26:05 +0530

    Load books inside ListBooks component using BookAPI call.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './App.css'
 import ListBooks from './ListBooks';
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+  //Fetch the books from the BooksAPI
+  componentDidMount(){
+    BooksAPI.getAll()
+    .then((books)=> {
+      this.setState({books})
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <ListBooks />
+        <ListBooks books={this.state.books}/>
       </div>
     )
   }
