a7a7082af413a7749aefe45ffeb9f2c0fb47c021
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-17 14:52:55 +0530

    Create ListBooks component with a dummy book.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import ListBooks from './ListBooks';
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-
+        <ListBooks />
       </div>
     )
   }
