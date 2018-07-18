7a2b9951bc2e620a6e203b661973d264a7605829
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-18 20:38:19 +0530

    Handle issue where books are returned without thumbnail link.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -61,7 +61,7 @@ class SearchBooks extends React.Component {
 						<li key={book.id}>
 							<div className="book">
 								<div className="book-top">
-									<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+									<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks && book.imageLinks.thumbnail)?book.imageLinks.thumbnail:''})` }}></div>
 										<div className="book-shelf-changer">
 											<select defaultValue={'none'} onChange={(event)=>{}}>
 												<option value="move" disabled>Move to...</option>
