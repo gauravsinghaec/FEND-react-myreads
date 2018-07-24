c0c62a3778e5138f532b0a39de498ce8250bd359
gauravsinghaec <gauravsinghaec@gmail.com>
2018-07-24 19:38:32 +0530

    refactor: Create new Book component to use in main and search.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 const ListBooks = ({
   shelf, books, title, onChangeShelf,
@@ -14,38 +15,7 @@ const ListBooks = ({
           <ol className="books-grid">
             {books.map(book => (
               book.shelf === shelf && (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks && book.imageLinks.thumbnail) ? book.imageLinks.thumbnail : ''})` }} />
-                    <div className="book-shelf-changer">
-                      <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={(event) => { onChangeShelf(book, event.target.value); }}>
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">
-                          Want to Read
-                        </option>
-                        <option value="read">
-                          Read
-                        </option>
-                        <option value="none">
-                          None
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">
-                    { book.title }
-                  </div>
-                  <div className="book-authors">
-                    { book.authors }
-                  </div>
-                </div>
-              </li>
+              <Book key={book.id} onChangeShelf={onChangeShelf} book={book} />
               )))}
           </ol>
         </div>
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 /**
  * React component for search page
@@ -105,38 +106,7 @@ class SearchBooks extends React.Component {
         <div className="search-books-results">
           <ol className="books-grid">
             { showBooks.map(book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks && book.imageLinks.thumbnail) ? book.imageLinks.thumbnail : ''})` }} />
-                    <div className="book-shelf-changer">
-                      <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={(event) => { onChangeShelf(book, event.target.value); }}>
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">
-                          Want to Read
-                        </option>
-                        <option value="read">
-                          Read
-                        </option>
-                        <option value="none">
-                          None
-                        </option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">
-                    { book.title }
-                  </div>
-                  <div className="book-authors">
-                    { book.authors }
-                  </div>
-                </div>
-              </li>
+              <Book key={book.id} onChangeShelf={onChangeShelf} book={book} />
             ))}
           </ol>
         </div>
